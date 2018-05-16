9f25f2d4689d7146accea9459a930696b8b5ec47
Zac Jones <zacjones93@gmail.com>
2018-05-16 16:03:15 -0700

    further fixes

--- 04-Add-Local-State-with-Redux-like-Reducers-Using-Recompose/src/index.js
+++ 04-Add-Local-State-with-Redux-like-Reducers-Using-Recompose/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { compose, lifecycle } from "recompose";
+import 'index.css'
 
 /*
 TITLE:
--- 06-Transform-Props-by-using-the-Recompose-mapProps-Higher-Order-Component/src/index.js
+++ 06-Transform-Props-by-using-the-Recompose-mapProps-Higher-Order-Component/src/index.js
@@ -1,7 +1,3 @@
-import React from "react";
-import { render } from "react-dom";
-import { withProps } from "recompose";
-
 /*
 TITLE:
 Lock Props using Recompose
@@ -10,23 +6,45 @@ DESCRIPTION:
 Learn how to use the ‘withProps’ higher order component to pre-fill a prop,
 unable to be overridden.
 */
-const { Component } = React;
 
-const HomeLink = withProps(({ query }) => ({ href: "#/?query=" + query }))("a");
-const ProductsLink = withProps({ href: "#/products" })("a");
-const CheckoutLink = withProps({ href: "#/checkout" })("a");
 
-const App = () => (
-  <div className="App">
-    <header>
-      <HomeLink query="logo">Logo</HomeLink>
-    </header>
-    <nav>
-      <HomeLink>Home</HomeLink>
-      <ProductsLink>Products</ProductsLink>
-      <CheckoutLink>Checkout</CheckoutLink>
-    </nav>
-  </div>
+import React from "react";
+import { render } from "react-dom";
+import { mapProps } from "recompose";
+import 'index.css';
+
+const User = ({ name, status }) =>
+  <div className="User">{name}—{status}</div>;
+
+const UserList = ({ users, status }) =>
+  <div className="UserList">
+    <h3>{status} users</h3>
+    {users && users.map((user) => <User {...user} />)}
+  </div>;
+
+const users = [
+  { name: "Tim", status: 'active' },
+  { name: "Bob", status: 'active' },
+  { name: "Joe", status: 'active' },
+  { name: "Jim", status: 'inactive' },
+];
+
+const filterByStatus = (status) => mapProps(
+  ({ users }) => ({
+    status,
+    users: users.filter(u => u.status === status)
+  })
 );
+
+const ActiveUsers = filterByStatus('active')(UserList);
+const InactiveUsers = filterByStatus('inactive')(UserList);
+const PendingUsers = filterByStatus('pending')(UserList);
+
+const App = () =>
+<div className="App">
+  <ActiveUsers users={users} />
+  <InactiveUsers users={users} />
+  <PendingUsers users={users} />
+</div>;
 
 render(<App />, document.getElementById("root"));
